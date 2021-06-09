cacf0b93499be440462ebd7655adb5cb77d4f718
Gert-Jan Spilstijns <gspilstijns@gmail.com>
2021-06-10 01:04:09 +0200

    Completed CRUD on Events, starting Communities

--- miniCRM/src/main/java/com/crm/miniCRM/controller/EventController.java
+++ miniCRM/src/main/java/com/crm/miniCRM/controller/EventController.java
@@ -1,12 +1,16 @@
 package com.crm.miniCRM.controller;
 
 import com.crm.miniCRM.dto.EventDto;
+import com.crm.miniCRM.dto.PersonDto;
 import com.crm.miniCRM.model.Event;
+import com.crm.miniCRM.model.Person;
 import com.crm.miniCRM.model.persistence.EventRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,13 +37,33 @@ public class EventController {
     @GetMapping("/new")
     public String newevent(Model model){
         model.addAttribute ( "event",new EventDto (  ) );
-        return "new-event";
+        return "events/new-event";
     }
     @PostMapping
     public String addevent(EventDto eventDto){
         eventRepository.save ( convertToEntity(eventDto) );
         return "redirect:/events";
     }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm( @PathVariable("id") long id, Model model ){
+        Event event = eventRepository.findById ( id );
+        model.addAttribute ( "event", convertToDto ( event ) );
+        return "events/update-event";
+
+    }
+    @PostMapping("/edit/{id}")
+    public String updateEvent( @PathVariable("id") long id, EventDto eventDto,
+                              BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            eventDto.setId(id);
+            return "persons/update-person";
+        }
+
+        eventRepository.save(convertToEntity ( eventDto ));
+        return "redirect:/events";
+    }
+
 
     protected EventDto convertToDto( Event entity) {
         EventDto dto = new EventDto (
